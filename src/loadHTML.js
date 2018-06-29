c275151245d20c5c51ee9ddb846b92f0308214a1
chen650 <chen650@usc.edu>
2018-06-29 12:48:43 -0400

    header and body text for every section

--- src/loadHTML.js
+++ src/loadHTML.js
@@ -1,9 +1,7 @@
 var numSections = Object.keys(globalVars.details.sections).length
 for (var i=0; i<numSections; i++) { 
-	if (globalVars.details.sections[i].type === "header") {
-		var txt = "<h1>" + globalVars.details.sections[i].text + "</h1>"
-	} else {
-		var txt = globalVars.details.sections[i].text
-	}
-	sections.innerHTML += "<section class='step'>" + txt + "</section>"
+	var header = globalVars.details.sections[i].header
+	var body = globalVars.details.sections[i].body
+	sections.innerHTML += "<section class='step'><h1>" + header + "</h1><br><p>" + body + "</p></section>"
 }
+	
